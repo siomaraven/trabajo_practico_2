7105006472e42d18340026c30147f9e95abb0636
parad <parad@Ryzen>
2024-05-01 20:51:07 -0300

    Commit de ejercicio 4 en su rama correspondiente

---
+++ src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -0,0 +1,98 @@
+package ar.edu.unju.fi.ejercicio4.model;
+
+import ar.edu.unju.fi.ejercicio4.constantes.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Jugador {
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaNacimiento;
+	private String nacionalidad;
+    private double estatura;
+    private double peso;
+    private Posicion posicion;
+
+	public Jugador(String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, double estatura,
+			double peso, Posicion posicion) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.posicion = posicion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public double getEstatura() {
+		return estatura;
+	}
+
+	public void setEstatura(double estatura) {
+		this.estatura = estatura;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
+	public int calcularEdad() {
+        LocalDate fechaActual = LocalDate.now();
+        Period periodo = Period.between(fechaNacimiento, fechaActual);
+        return periodo.getYears();
+    }
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento
+				+ ", nacionalidad=" + nacionalidad + ", estatura=" + estatura + ", peso=" + peso + ", posicion="
+				+ posicion + "]";
+	}
+	
+
+}
